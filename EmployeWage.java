a069a22316d85e6ea9d114275c3b0bb1084308ae
rahulraju69 <rahulraju9550@gmail.com>
2020-08-27 12:41:49 +0530

    checking employee present or not

---
+++ EmployeWage.java
@@ -0,0 +1,18 @@
+public class EmployeWage
+{
+        public static void main(String[] args)
+        {
+        int isPresent=1;
+        	int empCheck=(int)(Math.random()*10)%2;
+		System.out.println(empCheck);
+                	if(empCheck==isPresent)
+                	{
+                        	System.out.println("emp is present");
+                	}
+               		 else
+                	{
+                        	System.out.println("emp is absent");
+                	}
+        }
+}
+
